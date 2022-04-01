2d10844f53e43f051e015711c4410264379c262d
Lincoln Ruteski <lruteski@gmail.com>
2022-04-01 18:25:16 -0300

    criado o scritp do player e usado o input manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float vel = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * vel;
+    }
+}
