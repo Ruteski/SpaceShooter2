41b83462044b82851a69662493ff943e25f7dc6c
Lincoln Ruteski <lruteski@gmail.com>
2022-04-01 19:51:27 -0300

    aplicando velocidade ao tiro

--- Assets/Scripts/TiroController.cs
+++ Assets/Scripts/TiroController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class TiroController : MonoBehaviour
 {
+    [SerializeField] private float velocidade = 10f;
+    private Rigidbody2D meuRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
+
+        meuRB.velocity = new Vector2(0f, velocidade);
     }
 
     // Update is called once per frame
