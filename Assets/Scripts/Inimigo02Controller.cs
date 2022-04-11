2d0fd2645857847722bab50093473d11e30e72d6
Lincoln Ruteski <lruteski@gmail.com>
2022-04-11 17:10:05 -0300

    fazendo movimentacao down para o inimigo02

--- Assets/Scripts/Inimigo02Controller.cs
+++ Assets/Scripts/Inimigo02Controller.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Inimigo02Controller : InimigoPai
 {
+    private Rigidbody2D meuRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
+        //meuRB.velocity = new Vector2(0f, velocidade);
+        meuRB.velocity = Vector2.down * -velocidade;
     }
 
     // Update is called once per frame
