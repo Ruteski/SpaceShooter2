cd0cb5c228fee555a6f40e4fa2900e3a246f570b
Lincoln Ruteski <lruteski@gmail.com>
2022-04-16 13:52:11 -0300

    criando varios inimigos por vez

--- Assets/Scripts/GeradorInimigos.cs
+++ Assets/Scripts/GeradorInimigos.cs
@@ -29,20 +29,29 @@ public class GeradorInimigos : MonoBehaviour
         }
 
         if (esperaInimigo <= 0f) {
-            GameObject inimigoCriado;
+            //criando varios inimigos por vez
+            int quantidade = level * 4;
+            int qtdInimigos = 0;
 
-            //descidindo qual inimigo vai ser criado com base no level
-            float chance = Random.Range(0f, level);
+            while (qtdInimigos < quantidade) {
+                GameObject inimigoCriado;
 
-            if (chance > 2f) {
-                inimigoCriado = inimigos[1];
-            } else {
-                inimigoCriado = inimigos[0];
+                //descidindo qual inimigo vai ser criado com base no level
+                float chance = Random.Range(0f, level);
+
+                if (chance > 2f) {
+                    inimigoCriado = inimigos[1];
+                } else {
+                    inimigoCriado = inimigos[0];
+                }
+
+                Vector3 posicao = new Vector3(Random.Range(-8f, 9f), Random.Range(6f, 17f), 0f);
+                Instantiate(inimigoCriado, posicao, transform.rotation);
+                
+                qtdInimigos++;
+
+                esperaInimigo = tempoEspera;
             }
-
-            Vector3 posicao = new Vector3(Random.Range(-8f, 9f), Random.Range(6f, 17f), 0f);
-            Instantiate(inimigoCriado, posicao, transform.rotation);
-            esperaInimigo = tempoEspera;
         }
     }
 }
