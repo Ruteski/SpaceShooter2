032db573aa14ce32929e09c80a26ee8097d7b61d
Lincoln Ruteski <lruteski@gmail.com>
2022-04-23 16:26:16 -0300

    corrigindo duplicacao do gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private void Awake() {
+        // garantindo que so exista 1 gamemanger por vez
+        int qtd = FindObjectsOfType<GameManager>().Length;  
+
+        if (qtd > 1) {
+            Destroy(gameObject);    
+        }
+ 
         // nao serei destruido ao mudar de cena
         DontDestroyOnLoad(gameObject);    
     }
